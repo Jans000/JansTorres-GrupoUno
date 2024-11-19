e6eec16149ccab7834ea66620273e9d35e14b149
Jans000 <janstorres476@gmail.com>
2024-11-18 20:50:18 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import HomePage from './components/Home/Home';
 import AboutPage from './components/About/About';
 import ContactPage from './components/Contact/Contact';
 import ProductsPage from './components/Products/Products';
-import ItemDetail from './components/ItemDetail/ItemDetail';
+import Itemdetail from './components/Itemdetail/Itemdetail';
 import ProductsCategory from './components/Category/Category';
 import FormPage from './components/FormPage/FormPage';
 import Car from './components/Car/Car';
